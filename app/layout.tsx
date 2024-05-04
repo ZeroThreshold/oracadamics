18a284365d0f30158a9d915a6033424053c2e7ad
roverflow <roverflow5@gmail.com>
2024-05-04 17:48:10 +0530

    added video

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,9 +27,9 @@ export default function RootLayout({
         <main className="min-h-screen flex flex-col items-center">
           <div className="flex-1 w-full flex flex-col gap-20 items-center">
             {children}
-            <AppFooter />
           </div>
         </main>
+        <AppFooter />
       </body>
     </html>
   );
