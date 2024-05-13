931f9f8c6684532734a62ee3340b1c4b2535b9f6
roverflow <roverflow5@gmail.com>
2024-05-13 13:15:48 +0530

    changes to page

--- app/driftrschoolpune/page.tsx
+++ app/driftrschoolpune/page.tsx
@@ -52,25 +52,22 @@ const levelsData = [
 
 export default function ProDirtAdventure() {
   return (
-    <div>
+    <div className="container flex flex-col gap-20">
       {/* Landing */}
-      <div className="container mx-auto mt-10 md:mt-20">
-        <div className=" mb-10">
-          <div className="flex items-center justify-center mb-6">
+      <section>
+        <div className="mt-10 md:mt-20 mb-10">
+          <div className="flex flex-col md:flex-row items-center justify-center mb-6">
             <Image
               src={drpune}
               alt="ProDirt Pune Logo"
-              width={300} // adjust as needed
-              height={300} // adjust as needed
+              width={600} // adjust as needed
+              height={600} // adjust as needed
             />
-            <h1 className="text-4xl md:text-6xl font-extrabold uppercase text-gray-900 ml-4">
-              Drift R Pune
-            </h1>
           </div>
-          <h2 className="text-5xl md:text-5xl font-extrabold text-gray-900 mb-4">
-            About Us
+          <h2 className="text-3xl md:text-5xl font-extrabold text-gray-900 mb-4">
+            <span className="border-b border-b-black">About Us</span>
           </h2>
-          <p className="text-base md:text-lg text-gray-700">
+          <p className="text-base md:text-lg text-gray-700 text-justify">
             Flat Tracking, is a fun, emerging, and accessible form of
             motorcycling that is among the fastest-growing motorsports in the
             world. TVS Motor Company is launching the first Drift R School at
@@ -80,147 +77,81 @@ export default function ProDirtAdventure() {
             drifting under the guidance of highly skilled trainers in a safe and
             controlled environment.
           </p>
-          <Image
-            className="p-8 mt-4 mb-12"
-            src={prodirtPune}
-            alt="drift-r-pune"
-            width={1200}
-            height={800}
-          />
         </div>
-      </div>
-      {/* Mid Picture */}
-      <div className="w-full">
-        <Image
-          className="w-full"
-          src={img1}
-          alt="drift-r-pune"
-        />
-      </div>
-      {/* courses */}
-      <div className="container mx-auto mt-10 md:mt-20">
-        {levelsData.map((level, index) => (
-          <div key={index} className="flex items-center justify-center mb-6">
-            {index % 2 === 0 ? (
-              <>
-                <div className="w-1/3 ml-4">
-                  <Image
-                    src={level.image}
-                    alt="Image"
-                    width={500}
-                    height={300}
-                    className="rounded-lg"
-                  />
-                  {/* Additional information */}
-                  <div className="mt-4">
-                    <h4 className="text-xl font-semibold mb-2">
-                      What will I Learn?
-                    </h4>
-                    <ul className="list-arrow pl-6">
-                      {level.learn.map((item, index) => (
-                        <li key={index}>{item}</li>
-                      ))}
-                    </ul>
-                    <h4 className="text-xl font-semibold mt-4 mb-2">
-                      Things to carry
-                    </h4>
-                    <p>{level.carry}</p>
-                    <h4 className="text-xl font-semibold mt-4 mb-2">
-                      Inclusions
-                    </h4>
-                    <ul className="list-arrow pl-6">
-                      {level.inclusions.map((item, index) => (
-                        <li key={index}>{item}</li>
-                      ))}
-                    </ul>
-                    <h4 className="text-xl font-semibold mt-4 mb-2">
-                      Exclusions
-                    </h4>
-                    <p>{level.exclusions}</p>
-                  </div>
-                  {/* Additional information */}
+        <Image className="rounded-lg w-full" src={img1} alt="drift-r-pune" />
+      </section>
+
+      <section className="mb-20">
+        <div className="text-3xl md:text-5xl font-extrabold uppercase text-gray-900 mb-12">
+          <span className="border-b border-b-black">
+            Training Programs Offered
+          </span>
+        </div>
+        <div className="flex flex-col gap-20">
+          {levelsData.map((level, index) => (
+            <div key={index} className="flex flex-col md:flex-row mb-6 gap-10">
+              <div className="md:w-full">
+                <h3 className="text-2xl md:text-4xl font-bold mb-2">
+                  {level.heading}
+                </h3>
+                <p className="text-lg text-gray-700 text-justify">
+                  {level.text}
+                </p>
+                <div className="mt-4">
+                  <h4 className="text-xl font-semibold mb-2">
+                    What will I Learn?
+                  </h4>
+                  <ul className="list-arrow pl-6">
+                    {level.learn.map((item, index) => (
+                      <li key={index}>{item}</li>
+                    ))}
+                  </ul>
+                  <h4 className="text-xl font-semibold mt-4 mb-2">
+                    Things to carry
+                  </h4>
+                  <p className="text-justify">{level.carry}</p>
+                  <h4 className="text-xl font-semibold mt-4 mb-2">
+                    Inclusions
+                  </h4>
+                  <ul className="list-arrow pl-6">
+                    {level.inclusions.map((item, index) => (
+                      <li key={index}>{item}</li>
+                    ))}
+                  </ul>
+                  <h4 className="text-xl font-semibold mt-4 mb-2">
+                    Exclusions
+                  </h4>
+                  <p>{level.exclusions}</p>
                 </div>
-                <div className="w-1/3 ml-4">
-                  <h3 className="text-4xl font-bold mb-2">{level.heading}</h3>
-                  <p className="text-lg text-gray-700">{level.text}</p>
-                  <div className="mt-4 border border-gray-300 rounded-lg p-4">
-                    <h4 className="text-xl font-semibold mb-2">Details</h4>
-                    <div className="grid grid-cols-1 gap-y-4">
-                      <div>
-                        <strong>Duration:</strong>
-                        <p>Batch 1: {level.duration.batch1}</p>
-                        <p>Batch 2: {level.duration.batch2}</p>
-                      </div>
-                      <div>
-                        <strong>Pricing:</strong>
-                        <p>{level.pricing}</p>
-                      </div>
+                {/* Additional information */}
+              </div>
+              <div className="md:w-full">
+                <Image
+                  src={level.image}
+                  alt="Image"
+                  width={500}
+                  height={300}
+                  className="rounded-lg w-full"
+                />
+                <div className="mt-4 border border-gray-300 rounded-lg p-4">
+                  <h4 className="text-xl font-semibold mb-2">Details</h4>
+                  <div className="grid grid-cols-1 gap-y-4">
+                    <div>
+                      <strong>Duration:</strong>
+                      <p>Batch 1: {level.duration.batch1}</p>
+                      <p>Batch 2: {level.duration.batch2}</p>
+                    </div>
+                    <div>
+                      <strong>Pricing:</strong>
+                      <p>{level.pricing}</p>
                     </div>
                   </div>
                 </div>
-              </>
-            ) : (
-              <>
-                <div className="w-1/3 ml-4">
-                  <h3 className="text-4xl font-bold mb-2">{level.heading}</h3>
-                  <p className="text-lg text-gray-700">{level.text}</p>
-                  {/* Additional information */}
-                  <div className="mt-4">
-                    <h4 className="text-xl font-semibold mb-2">
-                      What will I Learn?
-                    </h4>
-                    <ul className="list-arrow pl-6">
-                      {level.learn.map((item, index) => (
-                        <li key={index}>{item}</li>
-                      ))}
-                    </ul>
-                    <h4 className="text-xl font-semibold mt-4 mb-2">
-                      Things to carry
-                    </h4>
-                    <p>{level.carry}</p>
-                    <h4 className="text-xl font-semibold mt-4 mb-2">
-                      Inclusions
-                    </h4>
-                    <ul className="list-arrow pl-6">
-                      {level.inclusions.map((item, index) => (
-                        <li key={index}>{item}</li>
-                      ))}
-                    </ul>
-                    <h4 className="text-xl font-semibold mt-4 mb-2">
-                      Exclusions
-                    </h4>
-                    <p>{level.exclusions}</p>
-                  </div>
-                  {/* Additional information */}
-                </div>
-                <div className="w-1/3 ml-8">
-                  <Image
-                    src={level.image}
-                    alt="Image"
-                    width={500}
-                    height={300}
-                    className="rounded-lg"
-                  />
-                  <div className="mt-4 border border-gray-300 rounded-lg p-4">
-                    <h4 className="text-xl font-semibold mb-2">Details</h4>
-                    <div className="grid grid-cols-1 gap-y-4">
-                      <div>
-                        <strong>Duration:</strong>
-                        <p>Batch 1: {level.duration.batch1}</p>
-                        <p>Batch 2: {level.duration.batch2}</p>
-                      </div>
-                      <div>
-                        <strong>Pricing:</strong>
-                        <p>{level.pricing}</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </>
-            )}
-          </div>
-        ))}
-      </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
   );
 }
